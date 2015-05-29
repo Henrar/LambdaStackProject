35a37565de7189b8b29d1197473c120a2ef36a01
Bartłomiej Kumor <aargh1@gmail.com>
2015-05-30 00:43:15 +0200

    Added basic word processing

--- LambdaStackJava/src/main/java/com/lambda/stack/WordProcessing.java
+++ LambdaStackJava/src/main/java/com/lambda/stack/WordProcessing.java
@@ -1,7 +1,27 @@
 package com.lambda.stack;
 
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.util.*;
+
+import java.util.List;
+import java.util.Properties;
+
 /**
  * Created by Henrar on 2015-05-29.
  */
 public class WordProcessing {
+    public void processWords(String [] inputWords) {
+        String text = "I am feeling very upset";
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, parse, sentiment");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        Annotation annotation = pipeline.process(text);
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence : sentences) {
+            String sentiment = sentence.get(SentimentCoreAnnotations.ClassName.class);
+            System.out.println(sentiment + "\t" + sentence);
+        }
+    }
 }
