5dee9c9c1ff3d97a0f9c0cf099999ca325dac008
jbytecoder <jbytecoder@gmail.com>
2015-06-11 00:00:34 +0200

    DatabaseHelper  preprared queries in file

--- LambdaStackJava/src/main/java/com/lambda/stack/DatabaseHelper.java
+++ LambdaStackJava/src/main/java/com/lambda/stack/DatabaseHelper.java
@@ -1,6 +1,19 @@
 package com.lambda.stack;
 
-import java.sql.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Henrar on 2015-05-14.
@@ -13,42 +26,156 @@ public class DatabaseHelper {
     static final private String USER = "spark";
     static final private String PASS = "spark";
 
-    public void connectToDatabase()
-    {
-        Connection conn = null;
-        Statement stmt = null;
+    public static void init() throws IOException {
+        loadStatementStore(DatabaseHelper.class.getClassLoader().getResourceAsStream("ps.store"));
         try{
-            
-            
-            DriverManager.registerDriver((Driver)Class.forName(JDBC_DRIVER).newInstance());
-            
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            stmt = conn.createStatement();
-            String sql;
-            sql = "SELECT * FROM TBL_HASHTAG";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while(rs.next()){
-                System.out.println(rs.getString(1));
-            }
-            rs.close();
-            stmt.close();
-            conn.close();
-        }catch(Exception e ){
+        DriverManager.registerDriver((Driver)Class.forName(JDBC_DRIVER).newInstance());
+        }catch(Exception e) {
             e.printStackTrace();
-        }finally{
-            try{
-                if(stmt!=null)
-                    stmt.close();
-            }catch(SQLException se){
-                se.printStackTrace();
+        }
+//        for(String s : statementStore.keySet()){
+//            System.out.println(s+" = "+statementStore.get(s));
+//        }
+    }
+    
+    private static Map<String,String> statementStore =  new HashMap<>();
+    public static String getStatement(String name) {
+        return statementStore.get(name);
+    }
+    private static void loadStatementStore(InputStream in) throws IOException {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            StringBuilder sb = new StringBuilder(100);
+            String line;
+            int hi = -1,sci=-1;
+            
+            while(true) {
+                line = br.readLine();
+                if(line==null) break;
+                hi = line.indexOf('#');
+                if(hi>=0) line = line.substring(0,hi).trim();
+                if(line.length() < 1) continue;
+                
+                sci = line.lastIndexOf(';');
+                if(sci>=0){
+                    line = line.substring(0,sci);
+                    sb.append(line);
+                    
+                    line = sb.toString();
+                    sb.delete(0, sb.length());
+                    sci = line.indexOf(':');
+                    statementStore.put(line.substring(0,sci), line.substring(sci+1,line.length()));
+                }else {
+                    sb.append(line);
+                }
             }
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
+            
+        } finally {
+            in.close();
+        }
+    }
+    
+    public static DatabaseHelper openDB() throws SQLException {
+        return new DatabaseHelper(DriverManager.getConnection(DB_URL, USER, PASS));
+    }
+    
+    private Connection c;
+    
+//            
+//            
+//            
+//            
+//            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+//            stmt = conn.createStatement();
+//            String sql;
+//            sql = "SELECT * FROM TBL_HASHTAG";
+//            ResultSet rs = stmt.executeQuery(sql);
+//
+//            while(rs.next()){
+//                System.out.println(rs.getString(1));
+//            }
+//            rs.close();
+//            stmt.close();
+//            conn.close();
+//        }catch(Exception e ){
+//            e.printStackTrace();
+//        }finally{
+//            try{
+//                if(stmt!=null)
+//                    stmt.close();
+//            }catch(SQLException se){
+//                se.printStackTrace();
+//            }
+//            try{
+//                if(conn!=null)
+//                    conn.close();
+//            }catch(SQLException se){
+//                se.printStackTrace();
+//            }
+//        }
+//    }
+
+    public DatabaseHelper(Connection c) {
+        this.c = c;
+    }
+    
+    public PreparedStatement createStatement(String name) throws SQLException {
+        return c.prepareStatement(statementStore.get(name));
+    }
+    
+    public int findHashTag(String tag) throws SQLException {
+        PreparedStatement ps = createStatement("FIND_HASHTAG");
+        ps.setString(1, tag);
+        
+        try(ResultSet rs = ps.executeQuery())
+        {
+            while(rs.next()){
+                return rs.getInt(1);
             }
+            return -1;
+        }
+        
+    }
+    
+    public int insertHashTag(String tag) throws SQLException {
+        PreparedStatement ps = 
+                c.prepareStatement(statementStore.get("I_HASHTAG"),
+                                   PreparedStatement.RETURN_GENERATED_KEYS);
+        ps.setString(1, tag);
+        ps.execute();
+                
+        try(ResultSet rs = ps.getGeneratedKeys()) {
+            while(rs.next()){
+                return rs.getInt(1);
+            }
+        } finally {
+            ps.close();
+        }       
+        
+        return -1;
+    }
+    
+    public List<String> listKeywords() throws SQLException {
+        PreparedStatement ps = createStatement("LIST_KEYWORDS");
+        List<String> l = new LinkedList();
+        
+        try(ResultSet rs = ps.executeQuery())
+        {
+            while(rs.next()){
+                l.add(rs.getString(1));
+            }
+           
+        }
+        ps.close();
+        return l;
+    }
+    
+    
+    public void close(){
+        try{
+        c.close();
+        }catch(SQLException e){
+            e.printStackTrace();
         }
     }
 }
--- LambdaStackJava/src/main/java/com/lambda/stack/Lambda.java
+++ LambdaStackJava/src/main/java/com/lambda/stack/Lambda.java
@@ -1,11 +1,13 @@
 package com.lambda.stack;
 
+import java.sql.SQLException;
 import org.apache.log4j.Level;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.apache.log4j.Logger;
-import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFlatMapFunction;
@@ -25,7 +27,7 @@ import twitter4j.Status;
 public final class Lambda {
 
     private static TwitterHelper twitterHelper;
-    private static DatabaseHelper databaseHelper;
+
 
     public static void main(String[] args) throws Exception {
         String sparkHome = "../../opt/spark";
@@ -49,10 +51,12 @@ public final class Lambda {
         }
         
         twitterHelper = new TwitterHelper();
-        databaseHelper = new DatabaseHelper();
+        DatabaseHelper.init();
         twitterHelper.configureTwitterCredentials();
-        databaseHelper.connectToDatabase();
+//        databaseHelper.connectToDatabase();
        
+        
+        
         Logger.getLogger("org").setLevel(Level.OFF);
         Logger.getLogger("akka").setLevel(Level.OFF);
 
@@ -76,46 +80,83 @@ public final class Lambda {
         // Tutaj się dane agregują buduje się zbiór np z ostniej minuty - i potem 
         // przesuwamy się nad nim co sekunde i przetwarzamy te same dane są przetwarzane
         // po kilka razy, ale za to jest ich wiecej
-        JavaPairDStream<String,Integer> tags = tweets.window(new Duration(window*1000), 
-                                                             new Duration(slide*1000) )
-                                                     .flatMapToPair(new PairFlatMapFunction<Status, String, Integer>() 
+         tweets = tweets.window(new Duration(window*1000), 
+                                new Duration(slide*1000) );
+        
+        
+        JavaPairDStream<String,Integer> tags= tweets.flatMapToPair(new PairFlatMapFunction<Status, String, Integer>() 
         {
             @Override
             public Iterable<Tuple2<String, Integer>> call(Status t) throws Exception {
                List<Tuple2<String,Integer>> l = new ArrayList<>(t.getHashtagEntities().length);
                
-//               boolean count=false;
-//               for( HashtagEntity he : t.getHashtagEntities()) {
-//                   if(he.getText().equals("iphone")){
-//                       count=true;
-//                       break;
-//                   }
-////                   l.add(new Tuple2<>(he.getText(),Integer.valueOf(1)));
-//               }
+               try {
+                   DatabaseHelper dh = DatabaseHelper.openDB();
+                   String txt = t.getText();
+                   for(String s : dh.listKeywords()){
+                       
+                       if(txt.contains(s)){
+                        l.add(new Tuple2<>(s,Integer.valueOf(1)));
+                       }
+                   }
+//                for( HashtagEntity he : t.getHashtagEntities()) {
+//                    String text = he.getText(); 
+//                    if(!text.matches("[a-zA-Z0-9]*")) continue;
+//                    
+//                    int id = dh.findHashTag(text);
+//                    if(id<0) {
+//                       id = dh.insertHashTag(text);
+//                    }
+//                    
+//                    l.add(new Tuple2<>(text.toUpperCase(),Integer.valueOf(1)));
+//                }
+                dh.close();
+               }catch(SQLException e ) {
+                   e.printStackTrace();
+               }
                
                
 //               if(count){
-               String s = t.getText();
-               if(s.contains("like")) {
-                   l.add(new Tuple2<>("like",Integer.valueOf(1)));
-               }
-               if(s.contains("love")) {
-                   l.add(new Tuple2<>("love",Integer.valueOf(1)));
-               }
-               if(s.contains("hate")) {
-                   l.add(new Tuple2<>("hate",Integer.valueOf(1)));
-               }
-               if(s.contains("take")) {
-                   l.add(new Tuple2<>("take",Integer.valueOf(1)));
-               }
-                if(s.contains("admire")) {
-                   l.add(new Tuple2<>("admire",Integer.valueOf(1)));
-               }
+//               String s = t.getText();
+//               if(s.contains("like")) {
+//                   l.add(new Tuple2<>("like",Integer.valueOf(1)));
+//               }
+//               if(s.contains("love")) {
+//                   l.add(new Tuple2<>("love",Integer.valueOf(1)));
+//               }
+//               if(s.contains("hate")) {
+//                   l.add(new Tuple2<>("hate",Integer.valueOf(1)));
+//               }
+//               if(s.contains("take")) {
+//                   l.add(new Tuple2<>("take",Integer.valueOf(1)));
+//               }
+//                if(s.contains("admire")) {
+//                   l.add(new Tuple2<>("admire",Integer.valueOf(1)));
+//               }
 //               }
                return l;
             }
         }
         );
+        
+        JavaPairDStream<String,String> tagtext = tweets.flatMapToPair(new PairFlatMapFunction<Status, String, String>() {
+            @Override
+            public Iterable<Tuple2<String, String>> call(Status t) throws Exception {
+               List<Tuple2<String,String>> l = new ArrayList<>(t.getHashtagEntities().length);
+               
+               for( HashtagEntity he : t.getHashtagEntities()) {
+                   l.add(new Tuple2<>(he.getText(),t.getText()));
+               }
+               
+               return l;
+            }
+        }).reduceByKey(new Function2<String, String, String>() {
+            @Override
+            public String call(String t1, String t2) throws Exception {
+                return t1+" "+t2;
+            }
+        });
+        
         
         JavaPairDStream<String,Integer> tagsc = tags.reduceByKey(new Function2<Integer, Integer, Integer>() {
 
@@ -125,7 +166,17 @@ public final class Lambda {
             }
         });
       
-        tagsc.print(30);
+        tagsc.foreach(new Function<JavaPairRDD<String, Integer>, Void>() {
+            @Override
+            public Void call(JavaPairRDD<String, Integer> t1) throws Exception {
+               Map<String,Integer> m = t1.collectAsMap();
+               for(String s : m.keySet()) {
+                   System.out.println("s: "+s+" "+m.get(s));
+               }
+                System.out.println("==== MARK ==== ");
+               return null;
+            }
+        });
        
         ssc.start();
         ssc.awaitTermination();
